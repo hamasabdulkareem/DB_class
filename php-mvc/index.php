f8927675bf69bc2cdc523ae802cf587647ff26e7
hamasabdulkareem <46087481+hamasabdulkareem@users.noreply.github.com>
2020-10-06 08:19:08 +0300

    Update index.php

--- php-mvc/index.php
+++ php-mvc/index.php
@@ -1,6 +1,7 @@
 <?php
 include("core/route.php");
 include("core/controllers.php");
+include("core/db.php");
 include("core/view.php");
 include("core/model.php");
 $url=isset($_GET['url'])?$_GET['url']:"home";
